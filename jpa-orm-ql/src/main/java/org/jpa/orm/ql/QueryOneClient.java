08be272a495e3ac50ab782953edbe25f3149a2b9
Romit Modi <modiromit7@gmail.com>
2021-03-06 15:15:53 +0530

    change for query language task

--- jpa-orm-ql/src/main/java/org/jpa/orm/ql/QueryOneClient.java
+++ jpa-orm-ql/src/main/java/org/jpa/orm/ql/QueryOneClient.java
@@ -20,12 +20,12 @@ public class QueryOneClient {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
 
-		Guide g1  = new Guide("Rajat Modi", BigDecimal.valueOf(100000), "01SEP1989");
-		Guide g2 = new Guide("Kunji Lal Agarwal", BigDecimal.valueOf(1000000),"05JUL1964");
-		
+		Guide g1 = new Guide("Rajat Modi", BigDecimal.valueOf(100000), "01SEP1989");
+		Guide g2 = new Guide("Kunji Lal Agarwal", BigDecimal.valueOf(1000000), "05JUL1964");
+
 		Student s1 = new Student("2021MAR06", "Romit", g1);
-		Student s2= new Student("e1", "newTest", null);
-		
+		Student s2 = new Student("e1", "newTest", null);
+
 		entityManager.persist(s1);
 		entityManager.persist(s2);
 		entityManager.persist(g2);
@@ -35,6 +35,16 @@ public class QueryOneClient {
 		        .createQuery("SELECT s.name,s.enrollmentId from Student s where s.guide is null");
 		List<Student> students = studentQuery.getResultList();
 		System.out.println(students);
+
+		Query guideQuery = entityManager.createQuery(
+		        "select g.name, g.staffId from Guide g WHERE NOT Exists ( SELECT 1 from Student s where s.guide=g)");
+		List<Guide> guides = guideQuery.getResultList();
+		System.out.println(guides);
+		
+		Query query2 =entityManager.createQuery(
+		        "select g from Guide g join g.students s WHERE s.name like 'R%'");
+		guides = query2.getResultList();
+		System.out.println(guides);
 		transaction.commit();
 		entityManager.close();
 		entityManagerFactory.close();
